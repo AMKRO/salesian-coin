2f7283a9dd2bf28763bdcaa50eaaf783f633366b
jaurb17@salesiansunbury.vic.edu.au <jaurb17@salesiansunbury.vic.edu.au>
2025-08-21 05:16:33 +0000

    Edited HOMEPAGE

--- client_code/HOMEPAGE/__init__.py
+++ client_code/HOMEPAGE/__init__.py
@@ -7,3 +7,11 @@ class HOMEPAGE(HOMEPAGETemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('SEND_PAGE')
+
+  def button_5_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('SETTINGS_PAGE')
