bc3e136553620a5c0cfc15e28882b8612528ea99
jaurb17@salesiansunbury.vic.edu.au <jaurb17@salesiansunbury.vic.edu.au>
2025-08-26 23:50:47 +0000

    Fixed up a few things and added the transactions page

--- client_code/TRANSACTIONS_PAGE/__init__.py
+++ client_code/TRANSACTIONS_PAGE/__init__.py
@@ -1,13 +1,17 @@
-from ._anvil_designer import RECEIVE_PAGETemplate
+from ._anvil_designer import TRANSACTIONS_PAGETemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class RECEIVE_PAGE(RECEIVE_PAGETemplate):
+class TRANSACTIONS_PAGE(TRANSACTIONS_PAGETemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('HOMEPAGE')
